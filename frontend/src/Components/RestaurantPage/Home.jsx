a223252eef59322657e2ee68ff7aaf73b137232d
sesha-gopalan <seshagopalanrs@gmail.com>
2024-10-19 10:59:13 +0530

    Create Home.jsx

---
+++ frontend/src/Components/RestaurantPage/Home.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import Navigator from './Navigator';
+import HomeDummy from './HomeDummy';
+import AllRestaurants from './AllRestaurants';
+import MainFooter from '../LandingPage/MainFooter';
+
+function Home() {
+    return (
+        <div>
+            <Navigator />
+            <HomeDummy />
+            <AllRestaurants />
+            <MainFooter />
+        </div>
+    );
+}
+
+export default Home;
